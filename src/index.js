d0b99cf3d9965de0a4a59d28477cd06dd08ab629
EricNavar <ericnavar@ufl.edu>
2021-11-08 22:41:15 -0500

    add custom theme, font add meta tags to index.html

--- src/index.js
+++ src/index.js
@@ -4,11 +4,37 @@ import FullStory from 'react-fullstory';
 import './index.css';
 import { Calculator } from './Calculator';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createMuiTheme } from '@mui/material/styles';
+
+//https://reacttraining.com/react-router/web/guides/quick-start
+
+
+//https://coolors.co/f2dfd7-fef9ff-d4c1ec-9f9fed-736ced
+const myTheme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#736ced',
+    },
+    secondary: {
+      main: '#9f9fed',
+    },
+    itemContainer: 'rgba(215,218,225,1)',
+    backgroundColor1: 'rgba(66,179,245,1)',
+    backgroundColor2: 'rgba(95,44,130,1)',
+    backgroundGradient: 'linear-gradient(rgba(66,179,245,1) 0%, rgba(95,44,130,1) 100%)',
+  },
+  typography: {
+    fontFamily: 'Poppins, \'Lato\', sans-serif'
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <FullStory org={process.env.REACT_APP_ORG_ID} />
-    <Calculator />
+    <ThemeProvider theme={myTheme}>
+      <Calculator />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
