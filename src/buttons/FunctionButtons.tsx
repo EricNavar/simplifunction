3ab7464c8ed3cfac059ba2c9c809467e832d8637
EricNavar <ericnavar@ufl.edu>
2021-11-24 22:02:13 -0500

    - move search field to FunctionButtonsWrapper - make FunctionButtons a pure function

--- src/buttons/FunctionButtons.tsx
+++ src/buttons/FunctionButtons.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import {
   Typography,
   Grid,
-  TextField,
-  InputAdornment,
   ListSubheader
 } from '@mui/material';
 import '../styling/Calculator.css';
-import { functions, conversionFunction, trigonometryFunction } from '../functions';
-import { SearchIcon } from '../assets/SearchIcon';
+import { conversionFunction, trigonometryFunction } from '../functions';
 import { ExcelFunctionCategory, ExcelFunction } from '../commonTypes';
 import { FunctionButton } from './FunctionButton';
 
@@ -27,24 +24,14 @@ function SectionHeader(props: SectionHeaderProps) {
 
 type FunctionButtonsProps = {
   mobile: boolean,
-  functionButtonOnClick: (excelFunction: ExcelFunction) => void
+  functionButtonOnClick: (excelFunction: ExcelFunction) => void,
+  searchedFunctions: Array<ExcelFunction>
 }
 
 export function FunctionButtons(props: FunctionButtonsProps) {
-  const { mobile, functionButtonOnClick } = props;
+  const { mobile, functionButtonOnClick, searchedFunctions } = props;
 
   React.useEffect(() => { }, [mobile]);
-
-  const [searchInput, setSearchInput] = React.useState('');
-  const [searchedFunctions, setSearchedFunctions] = React.useState(functions);
-
-  function onChangeSearchInput(e: React.ChangeEvent<HTMLInputElement>) {
-    setSearchInput(e.target.value);
-    if (e.target.value === '') {
-      setSearchedFunctions(functions);
-    }
-    setSearchedFunctions(functions.filter((f: ExcelFunction) => f.commonName.toLowerCase().includes(e.target.value)));
-  };
 
   const ExcelFunctionTypeArray = [
     ExcelFunctionCategory.Math,
@@ -57,76 +44,47 @@ export function FunctionButtons(props: FunctionButtonsProps) {
   ];
 
   return (
-    <Grid item container xs={12} sm={6} spacing={mobile ? 0 : 2} component='section'>
-      {!mobile &&
-        <Typography component="h2" variant='h4' style={{ width: '100%' }}>
-          Excel Functions
-        </Typography>
-      }
-      <div style={{ display: 'flex', marginBottom: 20 }}>
-        <div className='function-textfield'>
-          <TextField
-            id="function-search"
-            placeholder="Search function"
-            value={searchInput}
-            onChange={onChangeSearchInput}
-            variant="outlined"
-            size='small'
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-              style: {
-                borderRadius: 20
+    <Grid item container spacing={2} className="function-buttons-grid-container">
+      {ExcelFunctionTypeArray.map((functionType: ExcelFunctionCategory, index1: number) => {
+        const categorizedFunctions = searchedFunctions.filter((func: ExcelFunction) =>
+          func.category === functionType
+        );
+        if (categorizedFunctions.length === 0) {
+          return <></>
+        }
+        return (
+          <React.Fragment>
+            <SectionHeader>
+              {functionType}
+            </SectionHeader>
+            {categorizedFunctions.filter(
+              (f: ExcelFunction) => f.category === functionType).map((obj: ExcelFunction, index2: number) => {
+                return (
+                  <FunctionButton
+                    key={index2}
+                    label={obj.commonName}
+                    onClick={() => { functionButtonOnClick(obj) }}
+                  />
+                );
               }
-            }}
-          />
-        </div>
-      </div>
-      <Grid item container spacing={2} className="function-buttons-grid-container">
-        {ExcelFunctionTypeArray.map((functionType: ExcelFunctionCategory, index1: number) => {
-          const categorizedFunctions = searchedFunctions.filter((func: ExcelFunction) =>
-            func.category === functionType
-          );
-          if (categorizedFunctions.length === 0) {
-            return <></>
-          }
-          return (
-            <React.Fragment>
-              <SectionHeader>
-                {functionType}
-              </SectionHeader>
-              {categorizedFunctions.filter(
-                (f: ExcelFunction) => f.category === functionType).map((obj: ExcelFunction, index2: number) => {
-                  return (
-                    <FunctionButton
-                      key={index2}
-                      label={obj.commonName}
-                      onClick={ ()=> { functionButtonOnClick(obj) } }
-                    />
-                  );
-                }
-                )}
-            </React.Fragment>
-          )
-        })}
-        <SectionHeader>
-          Number Base Conversion
-        </SectionHeader>
-        <FunctionButton
-          label="Conversion functions"
-          onClick={() => { functionButtonOnClick(conversionFunction) }}
-        />
-        <SectionHeader>
-          Trigonometry functions
-        </SectionHeader>
-        <FunctionButton
-          label="Trigonometry functions"
-          onClick={() => { functionButtonOnClick(trigonometryFunction) }}
-        />
-      </Grid>
+              )}
+          </React.Fragment>
+        )
+      })}
+      <SectionHeader>
+        Number Base Conversion
+      </SectionHeader>
+      <FunctionButton
+        label="Conversion functions"
+        onClick={() => { functionButtonOnClick(conversionFunction) }}
+      />
+      <SectionHeader>
+        Trigonometry functions
+      </SectionHeader>
+      <FunctionButton
+        label="Trigonometry functions"
+        onClick={() => { functionButtonOnClick(trigonometryFunction) }}
+      />
     </Grid>
   );
 }
--- src/buttons/FunctionButtonsWrapper.tsx
+++ src/buttons/FunctionButtonsWrapper.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import {
+  Typography,
+  Grid,
+  TextField,
+  InputAdornment
+} from '@mui/material';
 import { FunctionButtons } from './FunctionButtons';
 import { FunctionButtonsAccordion } from './FunctionButtonsAccordion';
 import { ExcelFunction } from '../commonTypes';
+import { SearchIcon } from '../assets/SearchIcon';
+import { functions } from '../functions';
 
 type FunctionButtonsWrapperProps = {
   mobile: boolean,
@@ -12,10 +20,53 @@ function FunctionButtonsWrapper(props: FunctionButtonsWrapperProps) {
   const { mobile, functionButtonOnClick } = props;
   React.useEffect(() => { console.log("FunctionButtonsWrapper useEffect()") }, [mobile]);
 
-  const content = (<FunctionButtons
-    mobile={mobile}
-    functionButtonOnClick={functionButtonOnClick}
-  />);
+  const [searchInput, setSearchInput] = React.useState('');
+  const [searchedFunctions, setSearchedFunctions] = React.useState(functions);
+
+  function onChangeSearchInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchInput(e.target.value);
+    if (e.target.value === '') {
+      setSearchedFunctions(functions);
+    }
+    setSearchedFunctions(functions.filter((f: ExcelFunction) => f.commonName.toLowerCase().includes(e.target.value)));
+  };
+
+  const content = (
+    <Grid item container xs={12} sm={6} spacing={mobile ? 0 : 2} component='section'>
+      {!mobile &&
+        <Typography component="h2" variant='h4' style={{ width: '100%' }}>
+          Excel Functions
+        </Typography>
+      }
+      <div style={{ display: 'flex', marginBottom: 20 }}>
+        <div className='function-textfield'>
+          <TextField
+            id="function-search"
+            placeholder="Search function"
+            value={searchInput}
+            onChange={onChangeSearchInput}
+            variant="outlined"
+            size='small'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+              style: {
+                borderRadius: 20
+              }
+            }}
+          />
+        </div>
+      </div>
+      <FunctionButtons
+        mobile={mobile}
+        functionButtonOnClick={functionButtonOnClick}
+        searchedFunctions={searchedFunctions}
+      />
+    </Grid>
+  );
 
   if (mobile) {
     return (
